7d351a482a1fd77c5a646380acb6e2b33d9049d7
Daniel Kos <unodgs@gmail.com>
2020-07-29 12:33:31 +0200

    Fix accessing webpack variables in jest

--- jest.config.js
+++ jest.config.js
@@ -8,5 +8,10 @@ module.exports = {
     moduleNameMapper: {
         "\\.(css|less|sass|scss)$": "<rootDir>/web/mocks/style-mock.js",
         "\\.(gif|ttf|eot|svg)$": "<rootDir>/web/mocks/file-mock.js"
-    }
+    },
+    globals: {
+        "env": { 
+            SERVER_API_URL: 'http://localhost:8080'
+        }
+    }    
 };
