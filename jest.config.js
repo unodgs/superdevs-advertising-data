3aa005c629c99a2ac4e2b29db60322e8b5e027fd
Daniel Kos <unodgs@gmail.com>
2020-07-29 12:31:04 +0200

    Make jest working with scss imports

--- jest.config.js
+++ jest.config.js
@@ -5,4 +5,8 @@ module.exports = {
     },
     testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
     moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+    moduleNameMapper: {
+        "\\.(css|less|sass|scss)$": "<rootDir>/web/mocks/style-mock.js",
+        "\\.(gif|ttf|eot|svg)$": "<rootDir>/web/mocks/file-mock.js"
+    }
 };
